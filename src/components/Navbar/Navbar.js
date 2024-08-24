f7686da267a3f2e984e33debe1cedf07dcfd091b
Hrithik110 <Hrithikkumarce2021@gmail.com>
2024-08-24 15:21:06 +0530

    Button Folder update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../Button";
+import Button from "../Button/Button";
 import Logo from "../Logo";
 import Search from "../Search/Search";
 import styles from "./navbar.module.css";
